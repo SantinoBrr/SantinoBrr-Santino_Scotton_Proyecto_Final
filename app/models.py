84da8b27b2fef10eb2e52d8b573ebc75a51ba039
SantinoBrr <santinodarioscotton@gmail.com>
2024-10-31 17:49:56 -0300

    Finalizacion del proyecto final, guarda la fecha de cuando se creo un celular nuevo

--- app/models.py
+++ app/models.py
@@ -5,5 +5,7 @@ class Celular(models.Model):
     marca = models.CharField(max_length=20)
     modelo = models.CharField(max_length=20)
     tiempo_de_uso = models.IntegerField(validators=[MinValueValidator(0)])
+    fecha_de_guardado = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
-        return f'{self.marca} {self.modelo} '
+        return f'{self.marca} {self.modelo}'
